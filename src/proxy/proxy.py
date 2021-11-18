ee2f4cfb95733012d51d618e2925789d5a1cd381
Andre Gomes <andre3000pt@gmail.com>
2021-11-18 21:30:19 +0000

    SUB message and little refactoring

--- src/proxy/proxy.py
+++ src/proxy/proxy.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, final
 import zmq, os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import utils
@@ -9,10 +9,17 @@ to_confirm = {}
 messages = {}
 last_sub_id = -1
 
-def handle_put(message:List):
-    """message: [return address, "PUT", [Topic, Content]]"""
-    topic = message[2][0]
-    content = message[2][1]
+def handle_put(message:List) -> bytes:
+    """Handler for the PUT message
+
+    Args:
+        message (List): [PUT, Topic, Content]
+
+    Returns:
+        bytes: "OK" message
+    """
+    topic = message[1]
+    content = message[2]
 
     if(to_deliver.get(topic) != None):
         #Add message to messages list
@@ -32,7 +39,7 @@ def handle_put(message:List):
             to_deliver[topic][subscriber].append(content)
 
     print(f'handle_put: {messages}')
-    return [message[0], b'', b"OK"]
+    return b"OK"
 
 def handle_get(message: List):
     """message: [return address, "GET", [Subscriber ID, Topic]]"""
@@ -42,20 +49,38 @@ def handle_get(message: List):
     # Retorna
     return [message[0], b'', b"Not Implemented"]
 
-def handle_sub(message: List):
-    """message: [return address, "SUB", [Subscriber ID, Topic]]"""
-    sub_id = int(message[2][0])
-    topic = message[2][1]
+def handle_sub(message: List) -> bytes:
+    """Handler for the SUB message
+
+    Args:
+        message (List): [SUB, SUB_ID, TOPIC]
+
+    Raises:
+        Exception: When A SUB message arrives with SUB_ID larger than local count
+
+    Returns:
+        bytes: Response to send back
+    """
+    sub_id = int(message[1])
+    topic = message[2]
 
     if (sub_id == -1):
         global last_sub_id  # https://docs.python.org/3/reference/executionmodel.html#naming-and-binding
         last_sub_id += 1
         sub_id = last_sub_id
-        pass
+    elif (sub_id > last_sub_id):
+        raise Exception("Careful, subscriber using ID not given by Proxy")
 
-    # ID is taken cared of, need to update internal data
+    if(to_deliver.get(topic) == None):
+        to_deliver[topic] = {}
 
-    return [message[0], b'', b"OK\r\n" + str(sub_id).encode('utf-8')]
+    if(to_deliver[topic].get(sub_id) == None):
+        to_deliver[topic][sub_id] = []
+    else:
+        print(f'Subscription of SUBID {sub_id} to topic {topic} already exists')
+
+    print(f'to_deliver: {to_deliver}')
+    return b"OK\r\n" + str(sub_id).encode('utf-8')
 
 def handle_unsub(message: List):
     """message: [return address, "UNSUB", [Subscriber ID, Topic]]"""
@@ -73,22 +98,18 @@ def handle_cfm(message: List):
     # Retorna
     return [message[0], b'', b"Not Implemented"]
 
-def process_msg(message: List) -> List:
-    """ Returns: [return address, message type, message elements] """
-    message_type = message[1]
+def process_msg(ret_address, message: List) -> List:
+    message_type = message[0]
 
-    if(message_type=="PUT"):
-        return handle_put(message)
-    elif(message_type=="GET"):
-        return handle_get(message)
-    elif(message_type=="SUB"):
-        return handle_sub(message)
-    elif(message_type=="UNSUB"):
-        return handle_unsub(message)
-    elif(message_type=="CFM"):
-        return handle_cfm(message)
+    new_message = {
+        'PUT': handle_put,
+        'GET': handle_get,
+        'SUB': handle_sub,
+        'UNSUB': handle_unsub,
+        'CFM': handle_cfm
+    }[message_type](message)
 
-    return ""
+    return [ret_address, b'', new_message]
 
 def main():
     # Prepare our context and sockets
@@ -100,20 +121,24 @@ def main():
     poller = zmq.Poller()
     poller.register(socket, zmq.POLLIN)
 
-    # Switch messages between sockets
-    while True:
-        socks = dict(poller.poll())
+    try:
+        # Switch messages between sockets
+        while True:
+            socks = dict(poller.poll())
 
-        if socks.get(socket) == zmq.POLLIN:
-            message = socket.recv_multipart()  # https://zguide.zeromq.org/docs/chapter3/#The-Simple-Reply-Envelope
+            if socks.get(socket) == zmq.POLLIN:
+                message = socket.recv_multipart()  # https://zguide.zeromq.org/docs/chapter3/#The-Simple-Reply-Envelope
 
-            print(f'Message Received in Proxy - 1: {message[0]}\n2: {message[1]}\n3: {message[2]}')
+                print(f'Message Received in Proxy - 1: {message[0]}\n2: {message[1]}\n3: {message[2]}')
 
-            parsed_msg = utils.parse_message(message)
-            response = process_msg(parsed_msg)
+                response = process_msg(message[0], message[2].decode("utf-8").split('\r\n'))
 
-            socket.send_multipart(response)
-
+                socket.send_multipart(response)
+    except KeyboardInterrupt:
+        print(" W: interrupt received, stopping...")
+    finally:
+        socket.close()
+        context.term()
 
 if __name__ == "__main__":
     main()
--- src/subscriber/sub.py
+++ src/subscriber/sub.py
@@ -4,6 +4,7 @@
 #   Sends "Hello" to server, expects "World" back
 #
 import zmq
+sub_id = -1
 
 def main():
     #  Prepare our context and sockets
@@ -11,9 +12,13 @@ def main():
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://localhost:5550")
 
+    socket.send(b"SUB\r\n-1\r\nTOPIC TOPIC")
+    message = socket.recv()
+    sub_id = int(message.decode("utf-8").split('\r\n')[1])
+
     #  Do 10 requests, waiting each time for a response
-    for request in range(1, 10):
-        socket.send(b"SUB\r\n-1\r\nTOPIC TOPIC")
+    for request in range(1, 14):
+        socket.send(b"SUB\r\n"+str(sub_id).encode('utf-8')+b"\r\nTOPIC TOPIC")
         message = socket.recv()
         print(f"Received reply {request} [{message}]")
 
